cf2da84b8afa5387b7e96be5e9fe27f84f16734a
katehehehe <80514801+katehehehe@users.noreply.github.com>
2023-04-24 13:14:29 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const path = require("path");
 // Allow cross-origin requests with credentials from http://localhost:3000
 app.use(
   cors({
-    origin: "http://localhost:3000",
+//     origin: "http://localhost:3000",
     credentials: true,
   })
 );
